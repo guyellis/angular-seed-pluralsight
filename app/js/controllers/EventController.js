621ab031fb11e4795e48b9bb12f1aa07f9e3344a
Guy Ellis <wildfiction@gmail.com>
2013-11-12 10:12:42 -0700

    course changes

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -8,7 +8,30 @@ eventsApp.controller('EventController',
         $scope.event = {
             name: 'Angular Boot Camp',
             date: '1/1/2013',
-            time: '10:30 am'
+            time: '10:30 am',
+            imageUrl: '/img/angular.png',
+            sessions: [
+                {
+                    name: "Directives Masterclass",
+                    upVoteCount: 0
+                },
+                {
+                    name: "Scopes for fun and profit",
+                    upVoteCount: 0
+                },
+                {
+                    name: "Well behaved controllers",
+                    upVoteCount: 0
+                }
+            ]
+        }
+
+        $scope.upVoteSession = function(session){
+            session.upVoteCount++;
+        }
+
+        $scope.downVoteSession = function(session){
+            session.upVoteCount--;
         }
 
     });
